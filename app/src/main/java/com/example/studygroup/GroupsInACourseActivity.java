0b877c4ad02df0fc172439ec001880f96df30d71
Gil16 <gilbli166@gmail.com>
2018-05-19 19:17:11 +0300

    LOGIC: Added FAB to GroupsInACourse activity, the fab directs to CreateGroup activity

--- app/src/main/java/com/example/studygroup/GroupsInACourseActivity.java
+++ app/src/main/java/com/example/studygroup/GroupsInACourseActivity.java
@@ -1,7 +1,10 @@
 package com.example.studygroup;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class GroupsInACourseActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class GroupsInACourseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groups_in_acourse);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.createGroupFab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Click action
+                Intent intent = new Intent(GroupsInACourseActivity.this, CreateGroup.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
