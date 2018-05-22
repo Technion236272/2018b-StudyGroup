6dafe3c454911d8a2853a63f9ac05bfd28683f7f
adhamSaif <ssadham@campus.technion.ac.il>
2018-05-22 19:52:25 +0300

    DB: Working on the Create Group activity dealing with wrong input by showing alert dialogs.

--- app/src/main/java/com/example/studygroup/CreateGroup.java
+++ app/src/main/java/com/example/studygroup/CreateGroup.java
@@ -1,7 +1,10 @@
 package com.example.studygroup;
 
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.graphics.Color;
+import android.os.Message;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -30,35 +33,32 @@ public class CreateGroup extends AppCompatActivity {
     private Spinner year;
     private Spinner numOfParticipants;
     private Button createButton;
+    private DatabaseReference myRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_group);
-
-        groupSubject = (EditText)findViewById(R.id.groupSubject);
-        Location = (EditText)findViewById(R.id.Location);
-        day = (Spinner)findViewById(R.id.daySpinner);
-        month = (Spinner)findViewById(R.id.monthSpinner);
-        year = (Spinner)findViewById(R.id.yearsSpinner);
-        numOfParticipants = (Spinner)findViewById(R.id.NumOfParticipants);
-        createButton = (Button)findViewById(R.id.CreateGroup);
+        groupSubject = findViewById(R.id.groupSubject);
+        Location = findViewById(R.id.Location);
+        day = findViewById(R.id.daySpinner);
+        month = findViewById(R.id.monthSpinner);
+        year = findViewById(R.id.yearsSpinner);
+        numOfParticipants = findViewById(R.id.NumOfParticipants);
+        createButton = findViewById(R.id.CreateGroup);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference myRef = database.getReference();
+        myRef = database.getReference();
 
-        String[] participantsNum = new String[]{"Number Of Participants","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"};
+        String[] participantsNum = new String[]{"Number Of Participants","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"};
         final List<String> participantsNumList = new ArrayList<>(Arrays.asList(participantsNum));
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, participantsNumList)
         {
             @Override
             public boolean isEnabled(int position){
-                if(position == 0)
-                    return false;
-                else
-                    return true;
+                return position != 0;
             }
             @Override
             public View getDropDownView(int position, View convertView,
-                                        ViewGroup parent) {
+                                        @NonNull ViewGroup parent) {
                 View view = super.getDropDownView(position, convertView, parent);
                 TextView tv = (TextView) view;
                 if(position == 0){
@@ -76,49 +76,65 @@ public class CreateGroup extends AppCompatActivity {
 
         Integer[] daysArr = new Integer[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22
                                             ,23,24,25,26,27,28,29,30,31};
-        ArrayAdapter<Integer> daysAdapter = new ArrayAdapter<Integer>(this,android.R.layout.simple_spinner_item, daysArr );
+        ArrayAdapter<Integer> daysAdapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, daysArr );
         day.setAdapter(daysAdapter);
 
         Integer[] monthArr = new Integer[]{1,2,3,4,5,6,7,8,9,10,11,12};
-        ArrayAdapter<Integer> monthAdapter = new ArrayAdapter<Integer>(this,android.R.layout.simple_spinner_item, monthArr);
+        ArrayAdapter<Integer> monthAdapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, monthArr);
         month.setAdapter(monthAdapter);
 
 
         Integer[] yearsArr = new Integer[]{2018,2019};
-        ArrayAdapter<Integer> yearAdapter = new ArrayAdapter<Integer>(this,android.R.layout.simple_spinner_item, yearsArr);
+        ArrayAdapter<Integer> yearAdapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, yearsArr);
         year.setAdapter(yearAdapter);
 
+    }
 
-        createButton.setOnClickListener(new View.OnClickListener()
+    public void openAlertDialog(View view)
+    {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        String subject = groupSubject.getText().toString();
+        if(subject.length()==0)
         {
-            //TODO: get the user ID
-            @Override
-            public void onClick(View view)
-            {
-                String subject = groupSubject.getText().toString();
-                String location = Location.getText().toString();
-                String date = day.getSelectedItem() + "-" + month.getSelectedItem() + "-"+year.getSelectedItem();
-                int numOfPart = (int)numOfParticipants.getSelectedItem();
-                AlertDialog.Builder myAlert = new AlertDialog.Builder(getApplicationContext());
-                /*if(subject == null || subject.length()==0)
-                {
-                    myAlert.setMessage(R.string.subjectAlert).setPositiveButton(R.string.Ok, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
+            alertDialog.setTitle(R.string.subjectError);
+            alertDialog.setPositiveButton(R.string.Continue, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
 
-                        }
-                    }).show();
-                    return;
-                }*/
 
-                myRef.child("Groups").child(subject).child("Coures Name").setValue("235503");
-                myRef.child("Groups").child(subject).child("Group Participants").setValue(numOfPart);
-                myRef.child("Groups").child(subject).child("Location").setValue(location);
-                myRef.child("Groups").child(subject).child("Date").setValue(date);
+                }
+            }).show();
+            return;
+        }
 
-            }
-        });
+        String location = Location.getText().toString();
+        if (location .length()==0)
+        {
+            alertDialog.setTitle(R.string.locationError);
+            alertDialog.setPositiveButton(R.string.Continue, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
 
+                }
+            }).show();
+            return;
+        }
+
+        String date = day.getSelectedItem() + "-" + month.getSelectedItem() + "-"+year.getSelectedItem();
+        if(numOfParticipants.getSelectedItemPosition() == 0)
+        {
+            alertDialog.setTitle(R.string.participantsError);
+            alertDialog.setPositiveButton(R.string.Continue, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+
+                }
+            }).show();
+            return;
+        }
+
+        Integer numOfPart = Integer.parseInt((numOfParticipants.getSelectedItem().toString()));
+        myRef.child("Groups").child(subject).child("Coures Name").setValue("235503");
+        myRef.child("Groups").child(subject).child("Group Participants").setValue(numOfPart);
+        myRef.child("Groups").child(subject).child("Location").setValue(location);
+        myRef.child("Groups").child(subject).child("Date").setValue(date);
     }
 
 }
